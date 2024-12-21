55f83ed67c4d6e822799fd0ceccb0cc36ebc3d7b
Zarie1 <160772594+Zarie1@users.noreply.github.com>
2024-12-21 20:26:45 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
@@ -22,50 +23,40 @@ struct event_list {
 
 static LIST_HEAD(event_list_head);
 
-void new_event(void);
 
 static int __init hello_init(void) {
+	int i;
 	struct event_list *element;
 
 	if (num_of_greet == 0 || (num_of_greet >= 5 && num_of_greet <= 10)) {
-		printk(KERN_WARNING "Number is 0 or from 5 to 10\n");
+		pr_warn("Number is 0 or from 5 to 10\n");
 	}
 	if (num_of_greet > 10) {
 		BUG_ON(1);
 	}
-	int i;
+	
 	for (i = 0; i < num_of_greet; i++) {
-		printk(KERN_EMERG "Hello, World!\n");
 		if (i == 7) {
 			element = NULL;
-		}
-		else {
-			element = kmalloc(sizeof(struct event_list), GFP_KERNEL);
+		} else {
+			element = kmalloc(sizeof(*element), GFP_KERNEL);
 		}
 		BUG_ON(!element);
+
 		element->event_time = ktime_get();
 		list_add_tail(&element->list, &event_list_head);
+			pr_info("Hello, World!\n");
 	}
 	return 0;
 }
 
 static void __exit hello_exit(void) {
-	struct event_list *md, *tmp;
-	list_for_each_entry_safe(md, tmp, &event_list_head, list) {
-	printk(KERN_EMERG "Time: %lld\n", md->event_time);
-	list_del(&md->list);
-	kfree(md);
+	struct event_list *element, *tmp;
+	list_for_each_entry_safe(element, tmp, &event_list_head, list) {
+	pr_info("Time: %lld\n", element->event_time);
+	list_del(&element->list);
+	kfree(element);
 	}
-}
-
-void new_event(void) {
-	struct event_list *element = kmalloc(sizeof(struct event_list), GFP_KERNEL);
-	if (!element) {
-	printk(KERN_ERR "Failed to allocate memory for new event\n");
-	return;
-	}
-	element->event_time = ktime_get();
-	list_add_tail(&element->list, &event_list_head);
 }
 
 module_init(hello_init);
